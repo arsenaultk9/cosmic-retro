f80a9eb7b23b260e28857f469913de35efe0dfe9
Kevin Arsenault <arsenaultk9@gmail.com>
2020-07-04 13:45:52 -0400

    Corrected unit test.

--- front-end/src/App.test.tsx
+++ front-end/src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText('Create React App v4-beta example with TypeScript');
   expect(linkElement).toBeInTheDocument();
 });
