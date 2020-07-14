bb8c14583fae3b24167e8bffe82b8077f15dcc58
Kevin Arsenault <arsenaultk9@gmail.com>
2020-07-14 18:11:35 -0400

    Plugged in sockets.

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -14,6 +14,27 @@ CorsMiddleware.register(app);
 // Routes
 FeedbackRoutes.register(app);
 
-app.listen(5481, () => {
+
+// Socket.io
+import http from 'http'; // .Server(app);
+// set up socket.io and bind it to our
+// http server.
+import io from 'socket.io'; // (http);
+
+// Start server listening
+const server = app.listen(5481, () => {
     console.log('App is listening on port 5481! ');
+});
+
+const socket = io.listen(server);
+
+// whenever a user connects on port 3000 via
+// a websocket, log that a user has connected
+socket.on('connection', (socket: any) => {
+    console.log('a user connected');
+
+    // whenever we receive a 'message' we log it out
+    socket.on('message', (message: any) => {
+        console.log(message);
+    });
 });
--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -3,6 +3,11 @@ import Container from '@material-ui/core/Container';
 import { Typography } from '@material-ui/core';
 import FeedbacksContainer from './containers/feedback/feedbacks/FeedbacksContainer';
 
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:5481');
+socket.emit('message', 'Hello from client');
+
 export default function App() {
     return (
         <Container maxWidth='sm'>
